141f43e1877f159b2a80dbcd9545a046e5f977cb
mwangimuringi <mwangimuringi10@gmail.com>
2025-01-13 23:40:57 +0300

    Add DashboardHeader component for top navigation

---
+++ app/dashboard/components/DashboardHeader.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const DashboardHeader: React.FC = () => {
+  return (
+    <header>
+      <h1>Dashboard</h1>
+    </header>
+  );
+};
+
+export default DashboardHeader;
