9a3141f9c7357edaa6a76088fcfc922f66db4400
mwangimuringi <mwangimuringi10@gmail.com>
2025-01-31 12:49:27 +0300

    Created a basic notification component

---
+++ app/dashboard/components/Notification.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Notification = () => {
+  return (
+    <div>
+      This is a notification component
+      <button>Click me</button>
+    </div>
+  )
+}
+
+export default Notification
