1ea8578b6f9d67aa7dfab6bb5f48b869798bb633
mwangimuringi <mwangimuringi10@gmail.com>
2025-01-31 13:18:24 +0300

    Implemented login function in auth-hook.

--- app/dashboard/hooks/useAuth.ts
+++ app/dashboard/hooks/useAuth.ts
@@ -1,9 +1,14 @@
 import { useState } from "react";
 
 const useAuth = () => {
-  const [user, setUser] = useState<null | { id: string; name: string }>(null);
-
-  return { user };
-};
-
-export default useAuth;
+    const [user, setUser] = useState<null | { id: string; name: string }>(null);
+  
+    const login = (id: string, name: string) => {
+      setUser({ id, name });
+    };
+  
+    return { user, login };
+  };
+  
+  export default useAuth;
+  
