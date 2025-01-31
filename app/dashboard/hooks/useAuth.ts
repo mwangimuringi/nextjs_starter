c44faf50e9e965d2cd0fa6621eecee13f479ce37
mwangimuringi <mwangimuringi10@gmail.com>
2025-01-31 13:19:18 +0300

    Implement logout function.

--- app/dashboard/hooks/useAuth.ts
+++ app/dashboard/hooks/useAuth.ts
@@ -1,14 +1,13 @@
 import { useState } from "react";
 
 const useAuth = () => {
-    const [user, setUser] = useState<null | { id: string; name: string }>(null);
-  
-    const login = (id: string, name: string) => {
-      setUser({ id, name });
-    };
-  
-    return { user, login };
-  };
-  
-  export default useAuth;
-  
+  const [user, setUser] = useState<null | { id: string; name: string }>(null);
+
+  const login = (id: string, name: string) => setUser({ id, name });
+
+  const logout = () => setUser(null);
+
+  return { user, login, logout };
+};
+
+export default useAuth;
