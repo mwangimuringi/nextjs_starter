d422eb17df09b544c3a0ba0f9b78eaec190081db
mwangimuringi <mwangimuringi10@gmail.com>
2025-02-01 15:47:11 +0300

    Stored authentication token in localStorage.

--- app/dashboard/utils/auth.ts
+++ app/dashboard/utils/auth.ts
@@ -1,9 +1,13 @@
 import { post } from "./api";
 
 const login = async (email: string, password: string) => {
-  return post("/auth/login", { email, password });
-};
-
+    const response = await post("/auth/login", { email, password });
+    if (response.token) {
+      localStorage.setItem("token", response.token);
+    }
+    return response;
+  };
+  
 const signup = async (name: string, email: string, password: string) => {
     return post("/auth/signup", { name, email, password });
   };
