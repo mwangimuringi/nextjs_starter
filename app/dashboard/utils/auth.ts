f7473e1f9680d8260a7b65e23aa87eefa13500dc
mwangimuringi <mwangimuringi10@gmail.com>
2025-02-01 15:45:00 +0300

    Add signup function.

--- app/dashboard/utils/auth.ts
+++ app/dashboard/utils/auth.ts
@@ -4,4 +4,8 @@ const login = async (email: string, password: string) => {
   return post("/auth/login", { email, password });
 };
 
+const signup = async (name: string, email: string, password: string) => {
+    return post("/auth/signup", { name, email, password });
+  };
+  
 export { login };
