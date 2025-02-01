91553a63ab86fd0e79d7472f7624befc55724ba0
mwangimuringi <mwangimuringi10@gmail.com>
2025-02-01 15:45:43 +0300

    Implemented logout function.

--- app/dashboard/utils/auth.ts
+++ app/dashboard/utils/auth.ts
@@ -7,5 +7,9 @@ const login = async (email: string, password: string) => {
 const signup = async (name: string, email: string, password: string) => {
     return post("/auth/signup", { name, email, password });
   };
+
+  const logout = async () => {
+    return post("/auth/logout", {});
+  };  
   
 export { login };
