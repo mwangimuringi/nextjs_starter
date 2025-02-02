1917468515ee7bc3098f86c2e2f0bfd327157d38
mwangimuringi <mwangimuringi10@gmail.com>
2025-02-02 11:02:32 +0300

    Provided function to reset order state.

--- app/dashboard/hooks/useOrders.ts
+++ app/dashboard/hooks/useOrders.ts
@@ -1,17 +1,21 @@
 import { useState } from "react";
 
 const useOrder = () => {
-    const [order, setOrder] = useState(null);
-  
-    const createOrder = (orderData: any) => {
-      setOrder(orderData);
-    };
-    
-    const updateOrder = (updatedData: any) => {
-        setOrder((prevOrder) => ({ ...prevOrder, ...updatedData }));
-      };
-      
-      return { order, setOrder, createOrder, updateOrder };
-  };  
+  const [order, setOrder] = useState(null);
+
+  const createOrder = (orderData: any) => {
+    setOrder(orderData);
+  };
+
+  const updateOrder = (updatedData: any) => {
+    setOrder((prevOrder) => ({ ...prevOrder, ...updatedData }));
+  };
+
+  const resetOrder = () => {
+    setOrder(null);
+  };
+
+  return { order, setOrder, createOrder, updateOrder, resetOrder };
+};
 
 export default useOrder;
