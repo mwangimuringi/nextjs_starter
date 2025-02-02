b236a8c099bc21287d4631b6973b579c0db8f4b2
mwangimuringi <mwangimuringi10@gmail.com>
2025-02-02 10:55:43 +0300

    Added function to create a new order.

--- app/dashboard/hooks/useOrders.ts
+++ app/dashboard/hooks/useOrders.ts
@@ -1,9 +1,13 @@
 import { useState } from "react";
 
 const useOrder = () => {
-  const [order, setOrder] = useState(null);
-
-  return { order, setOrder };
-};
+    const [order, setOrder] = useState(null);
+  
+    const createOrder = (orderData: any) => {
+      setOrder(orderData);
+    };
+  
+    return { order, setOrder, createOrder };
+  };  
 
 export default useOrder;
