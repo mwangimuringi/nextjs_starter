269f8e7f10ced5c1fb04ee14d6d20adb359fd884
mwangimuringi <mwangimuringi10@gmail.com>
2025-02-05 23:51:01 +0300

    Initially setting up menu structure

---
+++ app/dashboard/pages/menu.tsx
@@ -0,0 +1,14 @@
+import Layout from "@/app/layout";
+
+const MenuPage = () => {
+  return (
+    <Layout>
+      <div className="p-6">
+        <h1 className="text-2xl font-bold">Menu</h1>
+        <p className="text-gray-600">Browse our delicious pizzas.</p>
+      </div>
+    </Layout>
+  );
+};
+
+export default MenuPage;
