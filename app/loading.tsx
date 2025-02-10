b944e9ed1fbf6dfa312140d5574af41f52085e46
mwangimuringi <mwangimuringi10@gmail.com>
2025-02-10 23:46:11 +0300

    FIxed hydration error

--- app/loading.tsx
+++ app/loading.tsx
@@ -2,12 +2,10 @@ import React from 'react'
 import Loader from './dashboard/components/Loader';
 
 const Loading = () => {
-    return <Loader />
-    <div className="flex items-center justify-center hc w-full bg-background z-20">
-        <div className="loader w-10 h-10"></div>
-        <div className="text-xl font-bold text-white">Loading...</div>
-        <div className="text-sm text-white">Please wait</div>
-    </div>
+    return <>
+    <Loader />
+    </>
+    <div className=""></div>
 };
 
 export default Loading
