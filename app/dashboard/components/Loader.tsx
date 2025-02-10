1a91ccc23686255b1a2736703efebfdd276a06a2
mwangimuringi <mwangimuringi10@gmail.com>
2025-02-10 23:44:09 +0300

    Added loaders component details

--- app/dashboard/components/Loader.tsx
+++ app/dashboard/components/Loader.tsx
@@ -4,6 +4,8 @@ const Loader = () => {
     return (
         <div className="flex items-center justify-center hc w-full bg-background z-20">
             <div className="loader w-10 h-10"></div>
+            <div className="text-xl font-bold text-white">Loading...</div>
+            <div className="text-sm text-white">Please wait</div>
         </div>
     )
 };
