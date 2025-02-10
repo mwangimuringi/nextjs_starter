ededc4296cbe0ee2259ad36d3b1739760315e938
mwangimuringi <mwangimuringi10@gmail.com>
2025-02-10 23:45:20 +0300

    Enhanced accessibility in components

--- app/loading.tsx
+++ app/loading.tsx
@@ -3,6 +3,11 @@ import Loader from './dashboard/components/Loader';
 
 const Loading = () => {
     return <Loader />
+    <div className="flex items-center justify-center hc w-full bg-background z-20">
+        <div className="loader w-10 h-10"></div>
+        <div className="text-xl font-bold text-white">Loading...</div>
+        <div className="text-sm text-white">Please wait</div>
+    </div>
 };
 
 export default Loading
