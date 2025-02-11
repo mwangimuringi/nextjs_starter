40090b9f06b0f9cd160d41fcec7f43a0d8f9f115
mwangimuringi <mwangimuringi10@gmail.com>
2025-02-11 18:10:16 +0300

    Addded the button component

--- app/dashboard/components/DashboardOrdersTable.tsx
+++ app/dashboard/components/DashboardOrdersTable.tsx
@@ -102,6 +102,26 @@ const DashboardOrdersTable: React.FC<Props> = ({ orders }) => {
             />
           </svg>
     </div>
+
+    <div className="flex justify-center">
+      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-6 w-6"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth={2}
+            d="M19.5 8.25l-7.5 7.5-7.5-7.5"
+          />
+        </svg>
+      </button>
+    </div> */}
+    </div>
   );
 };
 
