6e800feb30e81c4b3a6468fcfecf8763e237bb0f
mwangimuringi <mwangimuringi10@gmail.com>
2025-02-11 20:38:51 +0300

    ...

--- app/dashboard/components/DashboardOrdersTable.tsx
+++ app/dashboard/components/DashboardOrdersTable.tsx
@@ -83,6 +83,8 @@ const DashboardOrdersTable: React.FC<Props> = ({ orders }) => {
         <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
           View All
         </button>
+
+        
       </div>
 
       {/* TODO: Add pagination */}
