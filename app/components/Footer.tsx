d6cf495ad35cd047dcfce6ca7147ed9e9b5d09d1
mwangimuringi <mwangimuringi10@gmail.com>
2025-02-12 23:52:08 +0300

    Added navigation links

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,7 +1,20 @@
+import Link from "next/link";
+
 export default function Footer() {
-    return (
-        <footer className="bg-gray-800 text-white p-4 text-center">
-            &copy; {new Date().getFullYear()} CommerceHub. All rights reserved.
-        </footer>
-    );
+  return (
+    <footer className="bg-gray-800 text-white p-4 text-center">
+      <p>&copy; {new Date().getFullYear()} CommerceHub. All rights reserved.</p>
+      <div className="mt-2">
+        <Link href="/about" className="mx-2 hover:underline">
+          About
+        </Link>
+        <Link href="/contact" className="mx-2 hover:underline">
+          Contact
+        </Link>
+        <Link href="/privacy" className="mx-2 hover:underline">
+          Privacy Policy
+        </Link>
+      </div>
+    </footer>
+  );
 }
