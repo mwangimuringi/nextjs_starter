79595f37aea6165e8104f0b2a19c0fb727f0af1d
mwangimuringi <mwangimuringi10@gmail.com>
2025-02-12 23:37:53 +0300

    Added navigation links.

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,9 +1,22 @@
 import Link from "next/link";
 
 export default function Navbar() {
-    return (
-        <nav className="bg-blue-500 p-4 text-white flex justify-between">
-            <Link href="/">Home</Link>
-        </nav>
-    );
+  return (
+    <nav className="bg-blue-500 p-4 text-white flex justify-between">
+      <Link href="/" className="text-xl font-bold">
+        CommerceHub
+      </Link>
+      <div>
+        <Link href="/products" className="mx-2">
+          Products
+        </Link>
+        <Link href="/cart" className="mx-2">
+          Cart
+        </Link>
+        <Link href="/profile" className="mx-2">
+          Profile
+        </Link>
+      </div>
+    </nav>
+  );
 }
