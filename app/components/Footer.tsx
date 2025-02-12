4ec681f72c448205cb83458703c35700ea19099c
mwangimuringi <mwangimuringi10@gmail.com>
2025-02-12 23:52:46 +0300

    Improve Styling & Responsiveness

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -2,16 +2,16 @@ import Link from "next/link";
 
 export default function Footer() {
   return (
-    <footer className="bg-gray-800 text-white p-4 text-center">
+    <footer className="bg-gray-900 text-white p-6 text-center flex flex-col md:flex-row md:justify-between items-center">
       <p>&copy; {new Date().getFullYear()} CommerceHub. All rights reserved.</p>
-      <div className="mt-2">
-        <Link href="/about" className="mx-2 hover:underline">
+      <div className="mt-2 md:mt-0 space-x-4">
+        <Link href="/about" className="hover:underline">
           About
         </Link>
-        <Link href="/contact" className="mx-2 hover:underline">
+        <Link href="/contact" className="hover:underline">
           Contact
         </Link>
-        <Link href="/privacy" className="mx-2 hover:underline">
+        <Link href="/privacy" className="hover:underline">
           Privacy Policy
         </Link>
       </div>
