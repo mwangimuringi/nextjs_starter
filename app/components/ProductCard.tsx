42c8a67375a97e342f15b5bff050c585243e3021
mwangimuringi <mwangimuringi10@gmail.com>
2025-02-12 23:58:27 +0300

    Created a Simple Footer Component

---
+++ app/components/ProductCard.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+    return (
+        <footer className="bg-black text-gray-300 p-4 text-center">
+            <p>© {new Date().getFullYear()} CommerceHub. Your trusted marketplace.</p>
+        </footer>
+    );
+}
