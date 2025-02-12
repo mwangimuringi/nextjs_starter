60c3eb7429c7c3c70b58f9ce8f08c6c4c10a2bf6
mwangimuringi <mwangimuringi10@gmail.com>
2025-02-12 23:53:38 +0300

    Added social media icons

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import { FaFacebook, FaTwitter, FaInstagram } from "react-icons/fa";
 
 export default function Footer() {
   return (
@@ -14,6 +15,25 @@ export default function Footer() {
         <Link href="/privacy" className="hover:underline">
           Privacy Policy
         </Link>
+      </div>
+      <div className="mt-2 md:mt-0 flex space-x-3">
+        <a
+          href="https://facebook.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FaFacebook size={20} />
+        </a>
+        <a href="https://twitter.com" target="_blank" rel="noopener noreferrer">
+          <FaTwitter size={20} />
+        </a>
+        <a
+          href="https://instagram.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FaInstagram size={20} />
+        </a>
       </div>
     </footer>
   );
