e6328b87e138ba4880371e736d2afea8e0fe7d8f
mwangimuringi <mwangimuringi10@gmail.com>
2025-02-16 15:32:47 +0300

    Initialized cart page component.

---
+++ app/cart/page.tsx
@@ -0,0 +1,10 @@
+const CartPage = () => {
+    return (
+      <div className="p-6 max-w-2xl mx-auto bg-white rounded-xl shadow-md">
+        <h2 className="text-2xl font-semibold mb-4">Your Cart</h2>
+      </div>
+    );
+  };
+  
+  export default CartPage;
+  
