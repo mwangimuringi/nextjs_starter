7ccf4b96a8eac123d2645d9bfc061e6ae94e68dd
mwangimuringi <mwangimuringi10@gmail.com>
2025-02-17 18:40:28 +0300

    Initialized order details page.

---
+++ app/orders/[id]/page.tsx
@@ -0,0 +1,14 @@
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+
+const OrderDetailsPage = () => {
+    return (
+      <div>
+        <h2>Order Details</h2>
+        <p>Order information will be displayed here.</p>
+      </div>
+    );
+  };
+  
+  export default OrderDetailsPage;
+  
