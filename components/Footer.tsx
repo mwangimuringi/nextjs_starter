73e02c8f1ce7c2752d69febd72586bd081498af5
mwangimuringi <mwangimuringi10@gmail.com>
2025-02-19 11:14:05 +0300

    Made footer to be responsive

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,9 +2,11 @@ import Link from "next/link";
 
 const Footer = () => {
   return (
-    <footer className="bg-gray-800 text-white text-center py-4">
-      <p>© 2025 CommerceHub. All rights reserved.</p>
-      <div className="mt-2 space-x-4">
+    <footer className="bg-gray-800 text-white text-center py-4 md:flex md:justify-between md:px-8">
+      <p className="text-sm md:text-base">
+        © 2025 CommerceHub. All rights reserved.
+      </p>
+      <div className="mt-2 space-x-4 text-sm md:mt-0">
         <Link href="/about" className="hover:text-gray-400">
           About
         </Link>
