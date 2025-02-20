6c3b23200332a045e4625303731cbb043fda7d07
mwangimuringi <mwangimuringi10@gmail.com>
2025-02-20 19:16:53 +0300

    Add initial BidButton component

---
+++ components/BidButton.tsx
@@ -0,0 +1,8 @@
+// components/BidButton.tsx
+import React from "react";
+
+const BidButton: React.FC = () => {
+  return <button className="p-2 bg-blue-500 text-white rounded">Bid Now</button>;
+};
+
+export default BidButton;
