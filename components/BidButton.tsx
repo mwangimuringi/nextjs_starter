c19a4bff0c00d85d9ce4312a94fa0c298c6baef4
mwangimuringi <mwangimuringi10@gmail.com>
2025-02-20 19:18:03 +0300

    Add click handler to BidButton

--- components/BidButton.tsx
+++ components/BidButton.tsx
@@ -2,7 +2,15 @@
 import React from "react";
 
 const BidButton: React.FC = () => {
-  return <button className="p-2 bg-blue-500 text-white rounded">Bid Now</button>;
+  const handleClick = () => {
+    alert("Bid placed!");
+  };
+
+  return (
+    <button className="p-2 bg-blue-500 text-white rounded" onClick={handleClick}>
+      Bid Now
+    </button>
+  );
 };
 
 export default BidButton;
