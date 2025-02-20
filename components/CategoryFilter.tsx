cd6fe8ca4d7811bc66434c43899da2f66fb3cb15
mwangimuringi <mwangimuringi10@gmail.com>
2025-02-20 19:10:49 +0300

    Created the basic component

---
+++ components/CategoryFilter.tsx
@@ -0,0 +1,8 @@
+// components/CreateFilter.tsx
+import React from "react";
+
+const CreateFilter: React.FC = () => {
+  return <div className="p-4 border rounded-lg">Filter Component</div>;
+};
+
+export default CreateFilter;
