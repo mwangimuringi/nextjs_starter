a9f9262b42952ee1632750d863612cd57154291d
mwangimuringi <mwangimuringi10@gmail.com>
2025-02-20 19:11:55 +0300

    Added filter input field

--- components/CategoryFilter.tsx
+++ components/CategoryFilter.tsx
@@ -4,7 +4,11 @@ import React from "react";
 const CreateFilter: React.FC = () => {
   return (
     <div className="p-4 border rounded-lg shadow-md bg-gray-100">
-      Filter Component
+      <input
+        type="text"
+        placeholder="Search..."
+        className="w-full p-2 border rounded-md"
+      />
     </div>
   );
 };
