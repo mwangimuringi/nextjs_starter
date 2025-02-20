4570ee5c3db984eed4aaf11e213755e6e2b97adf
mwangimuringi <mwangimuringi10@gmail.com>
2025-02-20 19:11:20 +0300

    Added tailwind styling.

--- components/CategoryFilter.tsx
+++ components/CategoryFilter.tsx
@@ -2,7 +2,11 @@
 import React from "react";
 
 const CreateFilter: React.FC = () => {
-  return <div className="p-4 border rounded-lg">Filter Component</div>;
+  return (
+    <div className="p-4 border rounded-lg shadow-md bg-gray-100">
+      Filter Component
+    </div>
+  );
 };
 
 export default CreateFilter;
