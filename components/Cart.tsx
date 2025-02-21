ee6aaf227be7bbdb6bbb85bc0bc60e25f29b8f5e
mwangimuringi <mwangimuringi10@gmail.com>
2025-02-21 17:34:02 +0300

    Added remove functionality in cart component

--- components/Cart.tsx
+++ components/Cart.tsx
@@ -13,6 +13,10 @@ const Cart: React.FC = () => {
     { id: 2, name: "Product 2", price: 35 },
   ]);
 
+  const removeItem = (id: number) => {
+    setCartItems(cartItems.filter((item) => item.id !== id));
+  };
+
   return (
     <div className="p-4 border rounded-md">
       <h2 className="text-lg font-semibold">Shopping Cart</h2>
@@ -23,6 +27,7 @@ const Cart: React.FC = () => {
           {cartItems.map((item) => (
             <li key={item.id} className="flex justify-between">
               {item.name} - ${item.price}
+              <button className="text-red-500" onClick={() => removeItem(item.id)}>Remove</button>
             </li>
           ))}
         </ul>
