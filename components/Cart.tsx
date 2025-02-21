71d0562bd767297409f8364318846286a2c19ab0
mwangimuringi <mwangimuringi10@gmail.com>
2025-02-21 17:32:22 +0300

    Add initial Cart component

---
+++ components/Cart.tsx
@@ -0,0 +1,8 @@
+// components/Cart.tsx
+import React from "react";
+
+const Cart: React.FC = () => {
+  return <div className="p-4 border rounded-md">Your cart is empty.</div>;
+};
+
+export default Cart;
