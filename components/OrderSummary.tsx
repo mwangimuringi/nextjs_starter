b852168b1f59f4c77886d7b6e62c6afd34c91fad
mwangimuringi <mwangimuringi10@gmail.com>
2025-02-22 21:58:15 +0300

    Display the order items dynamically

--- components/OrderSummary.tsx
+++ components/OrderSummary.tsx
@@ -1,14 +1,24 @@
 interface OrderItem {
-    id: number;
-    name: string;
-    price: number;
-  }
-  
-  interface OrderSummaryProps {
-    items: OrderItem[];
-  }
-  
-  const OrderSummary: React.FC<OrderSummaryProps> = ({ items }) => {
-    return <div>Order Summary</div>;
-  };
-  
+  id: number;
+  name: string;
+  price: number;
+}
+
+interface OrderSummaryProps {
+  items: OrderItem[];
+}
+
+const OrderSummary: React.FC<OrderSummaryProps> = ({ items }) => {
+  return (
+    <div>
+      <h2>Order Summary</h2>
+      <ul>
+        {items.map((item) => (
+          <li key={item.id}>
+            {item.name} - ${item.price}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
