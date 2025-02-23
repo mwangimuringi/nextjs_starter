88bfdf0005a303eb02757ecd2a8f3f0993ae4bfc
mwangimuringi <mwangimuringi10@gmail.com>
2025-02-23 22:55:28 +0300

    Added CartItem type definition

--- hooks/useCart.ts
+++ hooks/useCart.ts
@@ -1,5 +1,12 @@
 import { useState } from "react";
 
+interface CartItem {
+    id: number;
+    name: string;
+    price: number;
+    quantity: number;
+  }  
+
 export const useCart = () => {
   const [cart, setCart] = useState([]);
   return { cart };
