c77c45ee8c52f35ca587a5bd3275b3f86ef52f49
mwangimuringi <mwangimuringi10@gmail.com>
2025-02-23 23:22:01 +0300

    Add CheckoutData type definition

--- hooks/useCheckout.ts
+++ hooks/useCheckout.ts
@@ -1,5 +1,11 @@
 import { useState } from "react";
 
+interface CheckoutData {
+    name: string;
+    email: string;
+    address: string;
+  }  
+
 export const useCheckout = () => {
   const [formData, setFormData] = useState({});
   return { formData };
