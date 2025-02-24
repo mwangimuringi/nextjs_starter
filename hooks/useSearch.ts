2745f81a912006ad064d70bfd4f571b8ab7056f4
mwangimuringi <mwangimuringi10@gmail.com>
2025-02-24 22:24:20 +0300

    Implemented handleSearch function

--- hooks/useSearch.ts
+++ hooks/useSearch.ts
@@ -3,5 +3,9 @@ import { useState } from "react";
 export const useSearch = () => {
   const [query, setQuery] = useState("");
 
+  const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setQuery(e.target.value);
+  };
+  
   return { query };
 };
