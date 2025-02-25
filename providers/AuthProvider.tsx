980eb63ff03ac8be6a147b9de38576f1e89a15e6
mwangimuringi <mwangimuringi10@gmail.com>
2025-02-25 22:45:13 +0300

    Initialized AuhProvider file

---
+++ providers/AuthProvider.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { useAuth } from '@commercehub/react-components';
+
+export const AuthProvider: React.FC = ({ children }) => {
+  const { isAuthenticated, login, logout } = useAuth();
+
+  return (
+    <div>
+      <div>
+        {isAuthenticated ? (
+          <button onClick={logout}>Logout</button>
+        ) : (
+          <button onClick={login}>Login</button>
+        )}
+      </div>
+      {children}
+    </div>
+  );
+};
