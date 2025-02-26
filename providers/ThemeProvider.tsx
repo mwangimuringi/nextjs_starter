65af52fcb5812f004d73e145e3f84174105ae379
mwangimuringi <mwangimuringi10@gmail.com>
2025-02-26 21:00:57 +0300

    Configured ThemeProvider with system preference support

--- providers/ThemeProvider.tsx
+++ providers/ThemeProvider.tsx
@@ -1,7 +1,11 @@
 import { ThemeProvider } from "next-themes";
 
 const CustomThemeProvider = ({ children }: { children: React.ReactNode }) => {
-  return <ThemeProvider attribute="class">{children}</ThemeProvider>;
+  return (
+    <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+      {children}
+    </ThemeProvider>
+  );
 };
 
 export default CustomThemeProvider;
